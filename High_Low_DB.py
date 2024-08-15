16c857f49d4384f8d95007f5a1da40427999449e
Rohit Trivedi <126136583+trivedi-rohit@users.noreply.github.com>
2024-08-16 00:26:28 +0530

    add Higher Lower game using function, & DB file.

---
+++ High_Low_DB.py
@@ -0,0 +1,134 @@
+data = [
+    {
+        'name':'Virat Kholi',
+        'follower_count': 270,
+        'description':'Cricketer',
+        'country':'India',
+    },
+    {
+        'name': 'ISRO',
+        'follower_count': 43,
+        'description': 'Space agency',
+        'country': 'India'
+    },
+    {
+        'name':'Dua Lipa',
+        'follower_count': 87,
+        'description':'singer',
+        'country':'United Kingdom',       
+    },
+    {
+        'name': 'Shawn Mendes',
+        'follower_count': 57,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'NASA',
+        'follower_count': 61,
+        'description': 'Space agency',
+        'country': 'United States'
+    },
+    {
+        'name': 'Billie Eilish',
+        'follower_count': 74,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Vin Diesel',
+        'follower_count': 103,
+        'description': 'Actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Shakira',
+        'follower_count': 123,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Real Madrid CF',
+        'follower_count': 86,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Katy Perry',
+        'follower_count': 174,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 149,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Khloé Kardashian',
+        'follower_count': 108,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 181,
+        'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'David Beckham',
+        'follower_count': 82,
+        'description': 'Footballer',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 167,
+        'description': 'Actor / TV personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 135,
+        'description': 'Magazine',
+        'country': 'United States'
+    },
+    {
+        'name': 'Sunil Chetri',
+        'follower_count': 45,
+        'description': 'Footballer',
+        'country': 'India'
+    },
+    {
+        'name': 'Maria Sharapova',
+        'follower_count': 27,
+        'description': 'Tennis',
+        'country': 'Russian '
+    },
+    {
+        'name': 'BARC',
+        'follower_count': 59,
+        'description': 'Nuclear Research Centre',
+        'country': 'India'
+    },
+    {
+        'name': 'Neymar',
+        'follower_count': 138,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'Narendra Modi',
+        'follower_count': 198,
+        'description': 'Politician',
+        'country': 'India'
+    },
+]
---
+++ Higher_Lower.py
@@ -0,0 +1,58 @@
+import random
+import os
+import High_Low_DB
+import High_Low_Art
+print(High_Low_Art.logo)
+score = 0
+
+def display_dict(information):
+    name = information["name"]
+    description = information["description"]
+    country = information["country"]
+    return (f"{name} is a {description} from {country}.")
+
+def check (guess, followers_1, followers_2):
+    if followers_1 < followers_2:
+        if guess == 1:
+            return False
+        else:
+            return True
+    else:
+        if guess==1:
+            return True
+        else:
+            return False
+
+choice_2 = random.choice(High_Low_DB.data)
+
+flag_status= True
+while flag_status:
+    choice_1 = choice_2
+    choice_2 = random.choice(High_Low_DB.data)
+    while choice_1 == choice_2:
+        choice_2 = random.choice(High_Low_DB.data)
+
+    # Displaying random choices form database.
+    print(f"Compare 1 : {display_dict(choice_1)}")
+    print(High_Low_Art.text2ascii_art)
+    print(f"Compare 2 : {display_dict(choice_2)}")
+
+    # Guess which has more follower
+    guess = int(input("\nWho has more followers? Type '1' or '2' : "))
+
+    #Fetch follower count from Database file (High_Low_DB)
+    follower_count_1 = choice_1["follower_count"]
+    follower_count_2 = choice_2["follower_count"]
+    print(follower_count_1)
+    print(follower_count_2)
+
+    Is_correct = check(guess, follower_count_1, follower_count_2)
+    os.system('cls')
+    print(High_Low_Art.logo)
+    if Is_correct == True:
+        score += 1
+        print(f"You are right. Your score is : {score}.\n")
+    else:
+        print(f"You are wrong. Your final score is {score}.\n")
+        flag_status = False
+
